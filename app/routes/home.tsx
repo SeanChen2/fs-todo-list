5e528c6f27c567bba42ff071905e0207e86e217f
Sean Chen <seanchen@seans-air.home>
2026-01-03 18:53:19 -0500

    Editable tasks

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import type { ActionFunctionArgs, LoaderFunctionArgs } from "react-router";
 import { TodoPage } from "../todo_page/TodoPage";
 import { requireUser } from "~/auth";
 import { db } from "~/db";
+import { FaBreadSlice } from "react-icons/fa6";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = await requireUser(request);
@@ -32,48 +33,79 @@ export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const actionType = formData.get("actionType");
   console.log("Action type: " + actionType)
-  
-  if (actionType === "add") {
-    const title = formData.get("newTask") as string;
 
-    if (!title.trim()) {
-      return { error: "Task title is required" };
-    }
-
-    await db.query(
-      `
-      INSERT INTO task (user_id, title)
-      VALUES ($1, $2)
-      `,
-      [user.id, title.trim()]
-    );
-
-  } else if (actionType === "toggle") {
-    const taskId = formData.get("taskId");
-    const completed = formData.get("completed");
-
-    await db.query(
-      `
-      UPDATE task
-      SET completed = $1
-      WHERE id = $2
-      `,
-      [completed, taskId]
-    );
-
-  } else if (actionType === "delete") {
-    const taskId = formData.get("taskId");
-    
-    await db.query(
-      `
-      DELETE FROM task
-      WHERE id = $1
-      `,
-      [taskId]
-    );
+  switch (actionType) {
+    case "add":
+      await addTask(formData, user);
+      break;
+    case "toggle":
+      await toggleTask(formData);
+      break;
+    case "delete":
+      await deleteTask(formData);
+      break;
+    case "edit":
+      await editTask(formData);
+      break;
   }
 
-  return null
+  return null;
+}
+
+async function addTask(formData: FormData, user: any) {
+  const title = formData.get("newTask") as string;
+
+  if (!title.trim()) {
+    return { error: "Task title is required" };
+  }
+
+  await db.query(
+    `
+    INSERT INTO task (user_id, title)
+    VALUES ($1, $2)
+    `,
+    [user.id, title.trim()]
+  );
+}
+
+async function toggleTask(formData: FormData) {
+  const taskId = formData.get("taskId");
+  const completed = formData.get("completed");
+
+  await db.query(
+    `
+    UPDATE task
+    SET completed = $1
+    WHERE id = $2
+    `,
+    [completed, taskId]
+  );
+}
+
+async function deleteTask(formData: FormData) {
+  const taskId = formData.get("taskId");
+    
+  await db.query(
+    `
+    DELETE FROM task
+    WHERE id = $1
+    `,
+    [taskId]
+  );
+}
+
+async function editTask(formData: FormData) {
+  const taskId = formData.get("taskId");
+  const newTitle = formData.get("newTitle");
+
+  await db.query(
+    `
+    UPDATE task
+    SET title = $1
+    WHERE id = $2
+    `,
+    [newTitle, taskId]
+  );
 }
 
 export default function Home() {
--- app/todo_page/TodoTask.tsx
+++ app/todo_page/TodoTask.tsx
@@ -1,7 +1,7 @@
-import React, { type ChangeEvent, type MouseEvent } from 'react';
+import React, { useState, type ChangeEvent, type FormEvent, type MouseEvent } from 'react';
 import type { Task } from './Interfaces';
 import { MdDelete } from "react-icons/md";
-import { useMantineTheme, ActionIcon, CloseButton, Checkbox } from '@mantine/core';
+import { useMantineTheme, ActionIcon, CloseButton, Checkbox, Text, TextInput } from '@mantine/core';
 import { useFetcher } from 'react-router';
 
 interface Props {
@@ -10,7 +10,8 @@ interface Props {
 
 export const TodoTask = ({task}: Props) => {
   const fetcher = useFetcher();
-  const theme = useMantineTheme();
+  const [editing, setEditing] = useState<boolean>(false);
+  const [newTaskName, setNewTaskName] = useState<string>(task.name);
 
   const onCheckboxToggle = (event: ChangeEvent<HTMLInputElement>) => {
     fetcher.submit(
@@ -32,6 +33,11 @@ export const TodoTask = ({task}: Props) => {
       { method: "post" }
     )
   }
+
+  const editTask = (event: FormEvent<HTMLFormElement>) => {
+    setEditing(false);
+    task.name = newTaskName;  // Optimistic update
+  }
   
   return (
     <div className='task'>
@@ -40,9 +46,34 @@ export const TodoTask = ({task}: Props) => {
         onChange={onCheckboxToggle}
       />
 
-      <div className='content'>
-        {task.name}
-      </div>
+      {editing ? (
+        // Edit mode
+        <fetcher.Form method="post" onSubmit={editTask}>
+          <TextInput
+            autoFocus
+            className='taskName'
+            placeholder='Enter a name for this task...'
+            name="newTitle"
+            value={newTaskName}
+            onChange={e => setNewTaskName(e.currentTarget.value)}
+            onBlur={() => setEditing(false)}
+          />
+
+          <input type="hidden" name="actionType" value="edit" />
+          <input type="hidden" name="taskId" value={task.id} />
+        </fetcher.Form>
+      ) : (
+        // Default mode (view)
+        <Text 
+          className={`taskName ${task.completed ? 'completed' : ''}`}
+          onClick={() => {
+            setEditing(true);
+            setNewTaskName(task.name);
+          }}
+        >
+          {task.name}
+        </Text>
+      )}
 
       <CloseButton 
         className="delete-btn" 
